e15be4fd4d8ce4f59e74988caa8b852aca19f4f1
Yokwejuste <yokwejuste@gmail.com>
2021-02-09 10:33:51 +0000

    Amazing, building a flower with the turtle GUI in python.

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import turtle
 
-qazi_turtle = turtle.Turtle()
-qazi_turtle.forward(100)
-qazi_turtle.right(75)
-qazi_turtle.forward(102)
-qazi_turtle
+hey = turtle.Turtle()
+
+def figure():
+  hey.left(70)
+  hey.forward(50)
+  hey.left(275)
+  hey.forward(50)
+  hey.right(95)
+  hey.forward(50)
+  hey.right(87)
+  hey.forward(70)
+
+
+for i in range(50):
+    figure()
